5460454a8f800757664d095be53bfbe1598c7ff6
yosemite <955804675@qq.com>
2018-04-08 23:11:13 +0800

    完成User subClass的编写

--- app/src/main/java/com/baby/project/projectbaby/sign/bean/User.java
+++ app/src/main/java/com/baby/project/projectbaby/sign/bean/User.java
@@ -10,7 +10,7 @@ import com.avos.avoscloud.AVObject;
  */
 
 @AVClassName("User")
-public class User extends AVObject{
+public class User extends AVObject {
 
     private static final String FIELD_ID_CARD = "id_card";
     private static final String FIELD_NAME = "name";
@@ -21,31 +21,69 @@ public class User extends AVObject{
     private static final String FIELD_BIRTHDAY = "birthday";
     private static final String FIELD_AVATAR = "avatar";
 
-    public void setIDCard(String idCard){
-        this.put(FIELD_ID_CARD,idCard);
+    public void setAvatar(String avatar) {
+        this.put(FIELD_AVATAR, avatar);
+    }
+
+    public String getAvatar() {
+        return this.getString(FIELD_AVATAR);
+    }
+
+    public void setBirthday(String birthday) {
+        this.put(FIELD_BIRTHDAY, birthday);
+    }
+
+    public String getBirthday() {
+        return this.getString(FIELD_BIRTHDAY);
+    }
+
+    public void setCertificates(String certificates) {
+        this.put(FIELD_CERTIFICATES, certificates);
+    }
+
+    public String getCertificates() {
+        return this.getString(FIELD_CERTIFICATES);
+    }
+
+    public void setAddress(String address) {
+        this.put(FIELD_ADDRESS, address);
+    }
+
+    public String getAddress() {
+        return this.getString(FIELD_ADDRESS);
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.put(FIELD_ETHNICITY, ethnicity);
+    }
+
+    public String getEthnicity() {
+        return this.getString(FIELD_ETHNICITY);
+    }
+
+    public void setIDCard(String idCard) {
+        this.put(FIELD_ID_CARD, idCard);
     }
 
     public String getIDCard() {
         return this.getString(FIELD_ID_CARD);
     }
 
-    public void setName(String name){
-        this.put(FIELD_NAME,name);
+    public void setName(String name) {
+        this.put(FIELD_NAME, name);
     }
 
     public String getName() {
         return this.getString(FIELD_NAME);
     }
 
-    public void setSex(boolean sex){
-        this.put(FIELD_SEX,sex);
+    public void setSex(boolean sex) {
+        this.put(FIELD_SEX, sex);
     }
 
     public boolean getSex() {
         return this.getBoolean(FIELD_SEX);
     }
-
-    
 
 
 }
